a10b4b0baacf5e0fabaa268e77d810684ebc803c
David <104325852+Cdaprod@users.noreply.github.com>
2023-04-02 15:06:40 -0400

    mobileTabBar

---
+++ javascript/vanillajs-mobileTabBar/README.md
@@ -0,0 +1,4 @@
+### This folder contains a mobile layout for a tab, bar and cards
+
+
+- changes made in data.json will be automatically reflected when previewing the index.html file
--- javascript/vanillajs-mobileTabBar/scripts.js
+++ javascript/vanillajs-mobileTabBar/scripts.js
@@ -1,21 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const tabButtons = document.querySelectorAll('.tab-button');
-    const cards = document.querySelectorAll('.card');
+    const tabBar = document.querySelector('.tab-bar');
+    const cardContainer = document.querySelector('.card-container');
 
-    tabButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            const targetTab = button.dataset.target;
-            
-            // Hide all cards
-            cards.forEach(card => {
-                card.hidden = true;
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            data.tabs.forEach(tab => {
+                // Create tab button
+                const tabButton = document.createElement('button');
+                tabButton.classList.add('tab-button');
+                tabButton.dataset.target = tab.id;
+                tabButton.textContent = tab.title;
+                tabBar.appendChild(tabButton);
+
+                // Create cards for the tab
+                tab.cards.forEach(cardContent => {
+                    const card = document.createElement('div');
+                    card.classList.add('card');
+                    card.dataset.tab = tab.id;
+                    card.textContent = cardContent;
+
+                    if (tab.id !== 'tab1') {
+                        card.hidden = true;
+                    }
+
+                    cardContainer.appendChild(card);
+                });
             });
 
-            // Show cards related to the selected tab
-            const selectedCards = document.querySelectorAll(`.card[data-tab="${targetTab}"]`);
-            selectedCards.forEach(card => {
-                card.hidden = false;
+            // Add click event listeners to the tab buttons
+            const tabButtons = document.querySelectorAll('.tab-button');
+            const cards = document.querySelectorAll('.card');
+
+            tabButtons.forEach(button => {
+                button.addEventListener('click', () => {
+                    const targetTab = button.dataset.target;
+
+                    // Hide all cards
+                    cards.forEach(card => {
+                        card.hidden = true;
+                    });
+
+                    // Show cards related to the selected tab
+                    const selectedCards = document.querySelectorAll(`.card[data-tab="${targetTab}"]`);
+                    selectedCards.forEach(card => {
+                        card.hidden = false;
+                    });
+                });
             });
-        });
-    });
+        })
+        .catch(error => console.error('Error fetching data:', error));
 });
